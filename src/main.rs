5fe80ec0a8c1d670c15a8ae5df90018b2023e2be
aspera-non-spernit <g.s.d>
2019-10-23 17:55:08 +0200

    chg --data for --input,  cli.yml

--- src/main.rs
+++ src/main.rs
@@ -215,7 +215,7 @@ fn main() -> std::io::Result<()> {
     let yaml = load_yaml!("/home/genom/Development/guru/cli.yml");
     let opts = App::from_yaml(yaml).get_matches();
     let error = f64::from_str(opts.value_of("error").unwrap()).unwrap();
-    let all_matches = if let Some(f) = opts.value_of("input") {
+    let all_matches = if let Some(f) = opts.value_of("data") {
         load_matches(f)?
     } else {
         // example matches
